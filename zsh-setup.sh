2f8e5efb9946c48590adf3b1404bc66b96e2d138
887 <2300887@gmail.com>
2017-07-24 20:05:13 +0200

    added zsh setup for prezto symlinks

---
+++ zsh-setup.sh
@@ -0,0 +1,8 @@
+#git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+
+ln -sf ~/.zprezto/runcoms/zprofile ~/.zprofile
+ln -sf ~/.zprezto/runcoms/zlogin ~/.zlogin
+ln -sf ~/.zprezto/runcoms/zlogout ~/.zlogout
+ln -sf ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -sf ~/.zprezto/runcoms/zshenv ~/.zshenv
+ln -sf ~/.zprezto/runcoms/zshrc ~/.zshrc
